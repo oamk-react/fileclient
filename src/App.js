6d6ef75c2e25baa2ba9dbae052d4da57fcef71b9
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-04-07 18:31:38 +0300

    Simple React client sending a image file and text to PHP file server.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [text, setText] = useState("");
+  
+  function save(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file',file);
+    formData.append('test', text);
+    fetch ('http://localhost:8888/fileserver/index.php',
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+    .then((res) => res.json())
+    .then ((result) => {
+      console.log(result);
+    })
+  };
+
+  function handleChange(e) {
+    setFile(e.target.files[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={save}>
+        <div>
+          <label>Image:</label>
+          <input type="file" name="file" onChange={handleChange}></input>
+          {file != null ?  (
+            <>
+              <p>Filename: {file.name}</p>
+              <p>Filetype: {file.type}</p>
+              <p>Filesize: {file.size}</p>
+            </>
+          ) : (
+            <p>File is not selected</p>
+          )}
+        </div>
+        <div>
+          <label>Text:</label>
+          <input type="text" name="text" value={text} onChange={e => setText(e.target.value)} />
+        </div>
+        <button>Save</button>
+      </form>
     </div>
   );
 }
